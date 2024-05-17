6dd0b5d767c5d4a9251a7d60b3ca2f7c496220b8
Prajwal S <prajwalplays@gmail.com>
2024-05-17 16:20:16 +0530

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,15 +3,19 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import "../../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../graphql/apollo-client";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider attribute='class' defaultTheme='default' enableSystem disableTransitionOnChange>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </ApolloProvider>
   );
 }
