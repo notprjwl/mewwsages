ffabbfbf9d15828a979b1a0b86d580b30026e295
Prajwal S <prajwalplays@gmail.com>
2024-05-20 22:08:38 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,26 @@
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import express from 'express';
 import http from 'http';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 
-async function main(typeDefs, resolvers) {
+async function main() {
   const app = express();
   const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs,
+
+  const schema = makeExecutableSchema({
+    typeDefs, 
     resolvers,
+  })
+
+  const server = new ApolloServer({
+    schema,
     csrfPrevention: true,
     cache: 'bounded',
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
   });
   await server.start();
   server.applyMiddleware({ app });
@@ -19,4 +28,5 @@ async function main(typeDefs, resolvers) {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
-main().catch(err => console.log(err));
+// wrapping the entire main in a catch block to see if any error occurs
+main().catch(err => console.log(err));  
