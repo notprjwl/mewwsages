185c99fd0e7a6e82b22a17f9a9a5df068edbabbd
Prajwal S <prajwalplays@gmail.com>
2024-05-22 19:54:17 +0530

    Create types.ts

---
+++ frontend/src/util/types.ts
@@ -0,0 +1,10 @@
+export interface CreateUsernameData {
+  createUsername: {
+    success: boolean;
+    error: boolean;
+  };
+}
+
+export interface CreateUsernameVariables {
+  username: string;
+}
