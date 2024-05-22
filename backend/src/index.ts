1412a31ea44e00c8fb6f8f0adbb627145b0f35ca
Prajwal S <prajwalplays@gmail.com>
2024-05-22 19:53:38 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,51 @@
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import express from 'express';
-import http from 'http';
-import typeDefs from './graphql/typeDefs';
-import resolvers from './graphql/resolvers';
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import express from "express";
+import http from "http";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+import { getSession } from "next-auth/react";
+import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
+import { GraphQlContext, Session } from "./util/types";
 
 async function main() {
+  dotenv.config();
   const app = express();
   const httpServer = http.createServer(app);
 
   const schema = makeExecutableSchema({
-    typeDefs, 
+    typeDefs,
     resolvers,
-  })
+  });
+
+  const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+  };
+
+  /**
+   * Context parameters
+   */
+  const prisma = new PrismaClient();
+  // const pubsub
 
   const server = new ApolloServer({
     schema,
     csrfPrevention: true,
-    cache: 'bounded',
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    cache: "bounded",
+    context: async ({ req, res }): Promise<GraphQlContext> => {
+      const session = await getSession({ req }) as Session; 
+      return { session, prisma };
+    },
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
   });
   await server.start();
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app, cors: corsOptions });
   await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
 // wrapping the entire main in a catch block to see if any error occurs
-main().catch(err => console.log(err));  
+main().catch((err) => console.log(err));
