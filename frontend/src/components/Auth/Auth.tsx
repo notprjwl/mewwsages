e1c36cbd09db7cc2b3e670673ea4ef1448e301e6
Prajwal S <prajwalplays@gmail.com>
2024-05-23 18:58:28 +0530

    Update Auth.tsx

--- frontend/src/components/Auth/Auth.tsx
+++ frontend/src/components/Auth/Auth.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import { Session } from "next-auth";
 import { useState } from "react";
@@ -7,6 +6,7 @@ import { useMutation } from "@apollo/client";
 import UserOperations from "@/src/graphql/operations/user";
 import { CreateUsernameData, CreateUsernameVariables } from "@/src/util/types";
 import toast from "react-hot-toast";
+import { Button } from "@radix-ui/themes";
 
 interface IAuthProps {
   session: Session | null;
@@ -53,15 +53,15 @@ const Auth: React.FC<IAuthProps> = ({ session, reloadSession }) => {
           <div className='justify-center flex flex-col items-center text-center w-50% gap-5'>
             <h1 className='text-2xl w-full'>Create a Username</h1>
             <Input type='text' placeholder='Enter a username' value={username} onChange={(e) => setUsername(e.target.value)} className='w-60' />
-            <Button variant='outline' onClick={onSubmit} className='bg-buttonColor opacity-70 text-text w-full'>
-              Submit
+            <Button variant="soft" loading={loading} onClick={onSubmit} className=' text-white bg-[#033300] outline-1 outline outline-[#27272a] hover:bg-[#1b1b1b] w-full hover: cursor-pointer'>
+              Save
             </Button>
           </div>
         </>
       ) : (
         <>
           <div className='text-2xl'>Mewwsages 🐈‍⬛</div>
-          <Button variant='outline' className='bg-buttonColor gap-2' onClick={() => signIn()}>
+          <Button loading={loading} className='bg-buttonColor gap-2 cursor-pointer hover:bg-buttonHover transition-all duration-200 ease-in-out' onClick={() => signIn()}>
             <img src='/images/googlelogo.png' alt='googleImg' height={20} width={20} className='bg-transparent' />
             Continue with Google
           </Button>
