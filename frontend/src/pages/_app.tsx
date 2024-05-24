3768295c29a5faf2e90b75a39e0d25a4208bc15b
Prajwal S <prajwalplays@gmail.com>
2024-05-24 17:42:46 +0530

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import { SessionProvider } from "next-auth/react";
-import { Theme } from "@radix-ui/themes";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import "../../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../graphql/apollo-client";
 import { Toaster } from "react-hot-toast";
-import { ThemeProvider } from "../components/theme-provider";
+import {ChakraProvider} from "@chakra-ui/react";
+import { theme } from "../chakra/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,12 +14,10 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
-        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
-          <Theme appearance='dark'>
+        <ChakraProvider theme={theme}>
             <Component {...pageProps} />
             <Toaster />
-          </Theme>
-        </ThemeProvider>
+        </ChakraProvider>
       </SessionProvider>
     </ApolloProvider>
   );
