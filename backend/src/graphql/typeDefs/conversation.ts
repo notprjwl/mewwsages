508f372efff0fa30e44f6ad9c594152254afb361
Prajwal S <prajwalplays@gmail.com>
2024-06-11 21:28:58 +0530

    Update conversation.ts

--- backend/src/graphql/typeDefs/conversation.ts
+++ backend/src/graphql/typeDefs/conversation.ts
@@ -1,13 +1,34 @@
 import { gql } from "apollo-server-core";
 
-const typeDefs = gql`
-    type Mutation {
-        createConversation(participantIds: [String]): CreateConversationResponse
-    }
 
-    type CreateConversationResponse {
-        conversationId: String,
-    }
+const typeDefs = gql`
+  scalar Date
+
+  type Mutation {
+    createConversation(participantIds: [String]): CreateConversationResponse
+  }
+
+  type CreateConversationResponse {
+    conversationId: String
+  }
+
+  type Conversation {
+    id: String
+    latestMessage: Message
+    participants: [Participant]
+    createdAt: Date
+    updatedAt: Date
+  }
+
+  type Participant {
+    id: String
+    user: User
+    hasSeenLatestMessage: Boolean
+  }
+
+  type Query {
+    conversations: [Conversation]
+  }
 `;
 
 export default typeDefs;
