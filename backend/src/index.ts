696b1d0bb596f5a9a5827c3eceb4674981c2540e
Prajwal S <prajwalplays@gmail.com>
2024-06-13 21:36:07 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,39 +7,82 @@ import typeDefs from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
 import { getSession } from "next-auth/react";
 import { PrismaClient } from "@prisma/client";
+import { PubSub } from "graphql-subscriptions";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
 import * as dotenv from "dotenv";
-import { GraphQlContext, Session } from "./util/types";
+import { GraphQlContext, Session, SubscriptionContext } from "./util/types";
 
 async function main() {
   dotenv.config();
   const app = express();
   const httpServer = http.createServer(app);
 
+  // Creating the WebSocket server
+  const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if app.use
+    // serves expressMiddleware at a different path
+    path: "/graphql/subscriptions",
+  });
+
+  // Hand in the schema we just created and have the
+  // WebSocketServer start listening.
+
   const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
   });
+
+  /**
+   * Context parameters
+   */
+  const prisma = new PrismaClient();
+  const pubsub = new PubSub();
+
+  const serverCleanup = useServer(
+    {
+      schema,
+      context: async (ctx: SubscriptionContext): Promise<GraphQlContext> => {
+        if (ctx.connectionParams && ctx.connectionParams.session) {
+          const { session } = ctx.connectionParams;
+          return { session, prisma, pubsub };
+        }
+        return { session: null, prisma, pubsub };
+      },
+    },
+    wsServer
+  );
 
   const corsOptions = {
     origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   };
 
-  /**
-   * Context parameters
-   */
-  const prisma = new PrismaClient();
-  // const pubsub
-
   const server = new ApolloServer({
     schema,
     csrfPrevention: true,
     cache: "bounded",
     context: async ({ req, res }): Promise<GraphQlContext> => {
-      const session = await getSession({ req }) as Session; 
-      return { session, prisma };
+      const session = (await getSession({ req })) as Session;
+      return { session, prisma, pubsub };
     },
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+    plugins: [
+      // Proper shutdown for the HTTP server.
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+
+      // Proper shutdown for the WebSocket server.
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
   });
   await server.start();
   server.applyMiddleware({ app, cors: corsOptions });
