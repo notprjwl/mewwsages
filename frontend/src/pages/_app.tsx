a2f7ec57d3514851cdd0377a80474e428c1845c3
Prajwal S <prajwalplays@gmail.com>
2024-06-17 20:03:09 +0530

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
 import "../../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../graphql/apollo-client";
@@ -8,7 +7,6 @@ import { Toaster } from "react-hot-toast";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
