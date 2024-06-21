1b3b4fd831381b1473ac3f09bb3e0dae05ad83af
Prajwal S <prajwalplays@gmail.com>
2024-06-22 00:04:52 +0530

    Create MessageItem.tsx

---
+++ frontend/src/components/Chat/Feed/Messages/MessageItem.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IMessageItemProps {
+}
+
+const MessageItem: React.FunctionComponent<IMessageItemProps> = (props) => {
+  return (
+    <div>
+    
+    </div>
+  )
+};
+
+export default MessageItem;
