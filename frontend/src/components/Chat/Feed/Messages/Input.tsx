32d3707cb65319ff44f132902275c39df40c78f9
Prajwal S <prajwalplays@gmail.com>
2024-06-25 20:20:29 +0530

    Update Input.tsx

--- frontend/src/components/Chat/Feed/Messages/Input.tsx
+++ frontend/src/components/Chat/Feed/Messages/Input.tsx
@@ -1,7 +1,11 @@
+import { useMutation } from "@apollo/client";
 import { Box, Input } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { useState } from "react";
+import { ObjectId } from "bson";
 import toast from "react-hot-toast";
+import MessageOperations from "../../../../graphql/operations/message";
+import { sendMessageArguments } from "../../../../../../backend/src/util/types";
 interface IMessageInputProps {
   session: Session;
   conversationId: string;
@@ -9,22 +13,44 @@ interface IMessageInputProps {
 
 const MessageInput: React.FunctionComponent<IMessageInputProps> = ({ session, conversationId }) => {
   const [messageBody, setMessageBody] = useState("");
+  const [sendMessage] = useMutation<{ sendMessage: boolean }, sendMessageArguments>(MessageOperations.Mutation.sendMessage);
 
   const onSendMessage = async (e: React.FormEvent) => {
     e.preventDefault();
 
     try {
-        // call sendMessage Mutation here
+      // call sendMessage Mutation here
+      const { id: senderId } = session.user;
+      // generate a new messageId
+      const messageId = new ObjectId().toString();
+      // object we are going to send to the backend
+      const newMessage: sendMessageArguments = {
+        id: messageId,
+        senderId,
+        conversationId,
+        body: messageBody,
+      };
+
+      const { data, errors } = await sendMessage({
+        variables: {
+          ...newMessage,
+        },
+      });
+
+      if (!data?.sendMessage || errors) {
+        throw new Error("FAILED TO SEND MESSAGE");
+      }
+
     } catch (error: any) {
-        console.log("onSendMessage error", error);
-        toast.error(error?.message);
+      console.log("onSendMessage error", error);
+      toast.error(error?.message);
     }
-  }
+  };
 
   return (
     <Box px={5} py={5} width='100%'>
-      <form onSubmit={() => {}}>
-        <Input value={messageBody} onChange={(e) => setMessageBody(e.target.value)} size='md' placeholder='New message' _focus={{ boxShadow: "none", border: "1px ", borderColor: "whiteAlpha.500" }} resize="none" />
+      <form onSubmit={onSendMessage}>
+        <Input value={messageBody} onChange={(e) => setMessageBody(e.target.value)} size='md' placeholder='New message' _focus={{ boxShadow: "none", border: "1px ", borderColor: "whiteAlpha.500" }} resize='none' />
       </form>
     </Box>
   );
