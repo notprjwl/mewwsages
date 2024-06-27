abf18164f392e0ea5f51143475f700ab89f70f0d
Prajwal S <prajwalplays@gmail.com>
2024-06-27 18:00:47 +0530

    Update Messages.tsx

--- frontend/src/components/Chat/Feed/Messages/Messages.tsx
+++ frontend/src/components/Chat/Feed/Messages/Messages.tsx
@@ -1,11 +1,13 @@
 import { MessageFields } from "@/src/graphql/operations/message";
-import { MessagesData, MessagesVariables } from "@/src/util/types";
+import { MessagesData, MessagesVariables, MessageSubscriptionData } from "@/src/util/types";
 import { useQuery } from "@apollo/client";
 import MessageOperations from "../../../../graphql/operations/message";
 import { Flex, Stack } from "@chakra-ui/react";
 import toast from "react-hot-toast";
 import MessageItem from "./MessageItem";
 import SkeletonLoader from "@/src/components/common/SkeletonLoader";
+import { subscribe } from "diagnostics_channel";
+import { useEffect } from "react";
 
 interface IMessagesProps {
   userId: string;
@@ -23,9 +25,64 @@ const Messages: React.FunctionComponent<IMessagesProps> = ({ userId, conversatio
     onCompleted: () => {},
   });
 
-  if(error) {
+  if (error) {
     return null;
   }
+
+  // const subscribeToMoreMessages = (conversationId: string) => {
+  //   return subscribeToMore({
+  //     document: MessageOperations.Subscription.messageSent,
+  //     variables: {
+  //       conversationId,
+  //     },
+  //     updateQuery: (prev, { subscriptionData }: MessageSubscriptionData) => {
+  //       if (!subscriptionData.data) return prev;
+
+  //       const newMessage = subscriptionData.data.messageSent;
+
+  //       return Object.assign({}, prev, {
+  //         messages:
+  //           newMessage.sender.id === userId
+  //             ? prev.messages
+  //             : [newMessage, ...prev.messages],
+  //       });
+  //     },
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   subscribeToMoreMessages(conversationId)
+  // }, [conversationId])
+  useEffect(() => {
+    const subscribeToMoreMessages = (conversationId: string) => {
+      return subscribeToMore({
+        document: MessageOperations.Subscription.messageSent,
+        variables: { conversationId },
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData?.data) return prev;
+
+          const newMessage = subscriptionData.data.messageSent;
+
+          // Check if the new message already exists in messages array
+          const alreadyExists = prev.messages.some((msg) => msg.id === newMessage.id);
+          if (alreadyExists) return prev;
+
+          // Append new message to messages array
+          return {
+            ...prev,
+            messages: [newMessage, ...prev.messages],
+          };
+        },
+      });
+    };
+
+    const unsubscribe = subscribeToMoreMessages(conversationId);
+
+    // Clean up subscription on component unmount
+    return () => {
+      unsubscribe();
+    };
+  }, [conversationId, subscribeToMore]);
 
   console.log("HERE IS THE MESSAGES DATA", data);
 
@@ -33,14 +90,14 @@ const Messages: React.FunctionComponent<IMessagesProps> = ({ userId, conversatio
     <Flex direction='column' justify='flex-end' overflow='hidden'>
       {loading && (
         <Stack spacing={3} px={5}>
-          <SkeletonLoader count={4} height="60px" />
+          <SkeletonLoader count={4} height='60px' />
         </Stack>
       )}
       {data?.messages && (
         <Flex direction='column-reverse' overflowY='scroll' height='100%'>
           {data.messages.map((message) => (
-            // <MessageItem />
-            <div key={message.id}>{message.body}</div>
+            <MessageItem key={message.id} message={message} sentByMe={message.sender.id === userId}/>
+            // <div key={message.id}>{message.body}</div>
           ))}
         </Flex>
       )}
