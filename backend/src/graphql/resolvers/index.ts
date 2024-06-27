cb712788d7c9205d196ffdf2f0ba35badcde7c97
Prajwal S <prajwalplays@gmail.com>
2024-06-27 17:59:52 +0530

    Update index.ts

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -1,8 +1,9 @@
+import merge from 'lodash.merge';
 import userResolvers from './user';
 import conversationResolvers from './conversation';
 import messageResolvers from './message';
-import merge from 'lodash.merge';
+import scalarResolvers from "./scalars"
 
-const resolvers = merge({}, userResolvers, conversationResolvers, messageResolvers);
+const resolvers = merge({}, userResolvers, conversationResolvers, messageResolvers, scalarResolvers);
 
 export default resolvers;
