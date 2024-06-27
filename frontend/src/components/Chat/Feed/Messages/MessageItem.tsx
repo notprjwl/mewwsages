279a70ff80c60f486518ac544610a818a386fa2c
Prajwal S <prajwalplays@gmail.com>
2024-06-27 18:00:43 +0530

    Update MessageItem.tsx

--- frontend/src/components/Chat/Feed/Messages/MessageItem.tsx
+++ frontend/src/components/Chat/Feed/Messages/MessageItem.tsx
@@ -1,14 +1,64 @@
-import * as React from 'react';
+import { Avatar, Box, Flex, Stack, Text } from "@chakra-ui/react";
+import { MessagePopulated } from "../../../../../../backend/src/util/types";
+import { formatRelative } from "date-fns";
+import { enUS } from "date-fns/locale/en-US";
 
 interface IMessageItemProps {
+  message: MessagePopulated;
+  sentByMe: boolean;
 }
 
-const MessageItem: React.FunctionComponent<IMessageItemProps> = (props) => {
+const formatRelativeLocale = {
+  lastWeek: "eeee",
+  yesterday: "Yesterday",
+  today: "p",
+  other: "MM/dd/yy",
+};
+
+const MessageItem: React.FunctionComponent<IMessageItemProps> = ({ message, sentByMe }) => {
   return (
-    <div>
-    
-    </div>
-  )
+    <Stack direction='row' p={4} spacing={2} wordBreak='break-word' justify={sentByMe ? 'flex-end' : 'flex-start'} >
+      {!sentByMe && (
+        <Flex align='flex-end'>
+          <Avatar size='xs' src={message.sender.image || ""} />
+        </Flex>
+      )}
+      <Stack spacing={1} width='100%'>
+        <Stack direction='row' align='center' justify={sentByMe ? 'flex-end' : 'flex-start'}>
+          {!sentByMe && (
+            <Text fontWeight={500} textAlign='left' fontSize='smaller'>
+              {message.sender.username}
+            </Text>
+          )}
+          <Text fontSize={13} align="center" color='whiteAlpha.700'>
+            {formatRelative(message.createdAt, new Date(), {
+              locale: {
+                ...enUS,
+                formatRelative: (token) => formatRelativeLocale[token as keyof typeof formatRelativeLocale],
+              },
+            })}
+          </Text>
+        </Stack>
+        <Flex justify={sentByMe ? 'flex-end' : 'flex-start'}>
+        <Box
+            bg={sentByMe ? "brand.200" : "whiteAlpha.200"}
+            px={4}
+            py={2}
+            borderRadius="20px"
+            maxWidth="65%"
+            _hover={{ bg: sentByMe ? "brand.100" : "whiteAlpha.300" }}
+            boxShadow="md"
+            borderTopRightRadius={sentByMe ? "20px" : "20px"}
+            borderTopLeftRadius={!sentByMe ? "20px" : "20px"}
+            borderBottomRightRadius={sentByMe ? "0" : "20px"}
+            borderBottomLeftRadius={!sentByMe ? "0" : "20px"}
+          >
+            <Text >{message.body}</Text>
+          </Box>
+        </Flex>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default MessageItem;
